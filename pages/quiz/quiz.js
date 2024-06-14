b8955b7361bd336c7b8fdcc510cfd8a22e392267
Arthur Zaffalon - Ciencia da Computação <foraalebr@gmail.com>
2024-06-14 17:58:34 +0000

    adicionada as funções do botão de próxima pergunta e de finalizar o quiz

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -97,7 +97,7 @@ function montarPergunta() {
                 </label>
             </form>
 
-            <button>Enviar</button>
+            <button>Responder</button>
         </section>`
 } 
 
@@ -117,6 +117,18 @@ function guardarResposta(evento) {
 
 function validarResposta() {
 
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.innerText = "Próxima"
+    botaoEnviar.removeEventListener("click", validarResposta)
+
+    if (pergunta === 10) {
+        botaoEnviar.innerText = "Finalizar"
+        botaoEnviar.addEventListener("click", finalizar)
+    } else {
+
+        botaoEnviar.addEventListener("click", proximaPergunta)
+    }
+
     if (resposta === quiz.questions[pergunta-1].answer) {
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
         pontos = pontos + 1
@@ -124,13 +136,22 @@ function validarResposta() {
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
         document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
     }
+
+    pergunta = pergunta + 1
 }
 
-async function iniciar() {
+function finalizar() {
+    localStorage.setItem("pontos", pontos)
 
-    alterarAssunto()
-    await buscarPerguntas()
+    window.location.href = "../resultado/resultado.html"
+}
+
+function proximaPergunta() {
     montarPergunta()
+    adicionarEventoInputs()
+}
+
+function adicionarEventoInputs() {
 
     const inputsResposta = document.querySelectorAll(".alternativas input")
     inputsResposta.forEach(input => {
@@ -141,6 +162,14 @@ async function iniciar() {
             respostaCorretaId = input.id
         }
     })
+}
+
+async function iniciar() {
+
+    alterarAssunto()
+    await buscarPerguntas()
+    montarPergunta()
+    adicionarEventoInputs()
 }
 
 iniciar()
